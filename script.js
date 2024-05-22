0141b0182bc4160c5d24f5b96a41426ed4be929b
ThomasNVu <139417064+ThomasNVu@users.noreply.github.com>
2024-05-22 15:42:35 -0400

    Write Pseudocode for getComputerChoice

---
+++ script.js
@@ -0,0 +1,13 @@
+/*Function getComputerChoice
+    Set randomNumberGenerator to Math.floor(Math.random() * 100)
+    Set rng to randomNumberGenerator mod 3
+    
+    If rng equals 0
+        Return "rock"
+    Else If rng equals 1
+        Return "paper"
+    Else
+        Return "scissors"
+    End If
+End Function
+*/
